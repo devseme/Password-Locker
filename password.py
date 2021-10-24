54d974d34d892adea29c79730b0f505ed0c950ff
devseme <ian.ochenge@student.moringaschool.com>
2021-10-24 11:49:02 +0300

    started on unittest

--- password.py
+++ password.py
@@ -2,6 +2,7 @@ class Password:
      '''
       initialized the instances 
      '''
+     password_list =[]
      def __init__(self,userName,userPassword):
          self.user_name=userName
          self.user_password=userPassword
