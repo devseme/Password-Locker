8b42745f54da0c38bba84eb34c0f1166d1edb18f
devseme <ian.ochenge@student.moringaschool.com>
2021-10-24 12:14:02 +0300

    ran the first test successfully

--- password_test.py
+++ password_test.py
@@ -1,2 +1,20 @@
-import unittest
-from password import Password
+import unittest #Importing the unittest module
+from password import Password #Importing the Password class
+
+class testPassword(unittest.TestCase):
+    '''
+    Class to test Password functions
+    ''' 
+    def setUp(self):
+        '''
+        '''
+        self.new_password=Password("ian","1234")
+
+    def test_init(self): 
+        self.assertEqual(self.new_password.user_name,"ian") 
+        self.assertEqual(self.new_password.user_password,"1234")   
+
+
+
+if __name__ ==  '__main__':
+    unittest.main()
