f3ae458c6719ed9259c8ae5c90079d8602205a4b
devseme <ian.ochenge@student.moringaschool.com>
2021-10-24 16:49:13 +0300

    ran  the first test for credentials class

--- credentials.py
+++ credentials.py
@@ -5,12 +5,11 @@ class Credentials:
 
 
      credential_list =[]
-     def __init__(self, user_name, account, account_username, user_password):
+     def __init__(self, user_name, account,user_password):
         '''
         defining the properties to be shown in credentials class
         '''
         self.user_name=user_name
         self.account=account
-        self.account_username=account_username
         self.user_password=user_password
          
--- credentials_test.py
+++ credentials_test.py
@@ -5,3 +5,15 @@ class testCredentials(unittest.TestCase):
     '''
     Class to test Credentials functions
     '''   
+    def setUp(self):
+        '''
+        '''
+        self.new_credentials=Credentials("ian","twitter","1234")
+
+    def test_init(self): 
+        self.assertEqual(self.new_credentials.user_name,"ian") 
+        self.assertEqual(self.new_credentials.account,"twitter")  
+        self.assertEqual(self.new_credentials.user_password,"1234")  
+
+if __name__ ==  '__main__':
+    unittest.main()
