95b1ec67bcdb01bcbc5c38d888418af0969597fc
devseme <ian.ochenge@student.moringaschool.com>
2021-10-24 11:13:30 +0300

    declared what our class will have

--- password.py
+++ password.py
@@ -1,4 +1,7 @@
 class Password:
      '''
       initialized the instances 
-      '''
+     '''
+     def __init__(self,userName,userPassword):
+         self.user_name=userName
+         self.user_password=userPassword
