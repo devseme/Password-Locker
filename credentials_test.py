c26eb4662a66e89ab53272705d5e2e4c2b6ad62b
devseme <ian.ochenge@student.moringaschool.com>
2021-10-24 17:01:47 +0300

    ran the third test for credentials class

--- credentials_test.py
+++ credentials_test.py
@@ -15,10 +15,19 @@ class testCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.account,"twitter")  
         self.assertEqual(self.new_credentials.user_password,"1234") 
 
+    def tearDown(self):
+        Credentials.credentials_list =[]
+
     def test_saving(self):
         self.new_credentials.save_credentials()
         print(Credentials.credentials_list)
         self.assertEqual(len(Credentials.credentials_list),1)
+
+    def test_multiple_saves(self):
+        self.new_credentials.save_credentials()
+        another_credentials =Credentials("justine","Instagram","12345")
+        another_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)    
 
 
 if __name__ ==  '__main__':
